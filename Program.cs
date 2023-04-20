b0f2ca3197b27832e9d2c7be45d7addfe85d2c1c
JimBraiden <127128682+JimBraiden@users.noreply.github.com>
2023-04-20 20:06:18 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты x1: ");
+int x1 = int.Parse(Console.ReadLine());
 
-int i = 1;
+Console.WriteLine("Введите координаты y1: ");
+int y1 = int.Parse(Console.ReadLine());
 
-for (i = 1; i <= number; i++)
-{
-int k = i * i * i;
-Console.Write($"{k}, ");
-}
+Console.WriteLine("Введите координаты z1: ");
+int z1 = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Введите координаты x2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+
+double answer = Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) * 1.0), 0.5);
+Console.WriteLine(answer);
